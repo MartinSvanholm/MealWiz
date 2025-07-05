116cbf98b62eccb2a7811db5aafcbe148eede92c
MartinSvanholm <mart373e@gmail.com>
2025-07-05 19:44:39 +0200

    Working WASM with routes

--- MealWiz/MealWizWASM/Program.cs
+++ MealWiz/MealWizWASM/Program.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.Authorization;
 using MealWizWASM.Providers;
+using Supabase;
+using MudBlazor.Services;
+using Features.Services.DrawerStateContainer;
+using MealWizFeatures.Services.DrawerStateContainer;
+using MealWizFeatures.Features.Meals.State;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,8 +15,26 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddScoped<IDrawerStateContainer, DrawerStateContainer>();
+builder.Services.AddScoped<IMealsStateContainer, MealsStateContainer>();
 
+builder.Services.AddMudServices();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(MealWizFeatures.Main.Layout).Assembly));
+
+ServiceProvider serviceProvider = builder.Services.BuildServiceProvider();
+
+string supabaseUrl = builder.Configuration["Supabase:url"];
+string supabaseKey = builder.Configuration["Supabase:key"];
+builder.Services.AddScoped(provider =>
+{
+    return new Client(supabaseUrl, supabaseKey, new SupabaseOptions
+    {
+        AutoRefreshToken = true,
+        AutoConnectRealtime = true,
+        SessionHandler = new CustomSupabaseSessionProvider()
+    });
+});
 
 await builder.Build().RunAsync();
---
+++ MealWiz/MealWizWASM/Providers/CustomSupabaseSessionProvider.cs
@@ -0,0 +1,25 @@
+﻿using Blazored.LocalStorage;
+using Newtonsoft.Json;
+using Supabase.Gotrue;
+using Supabase.Gotrue.Interfaces;
+
+namespace MealWizWASM.Providers
+{
+    public class CustomSupabaseSessionProvider() : IGotrueSessionPersistence<Session>
+    {
+        private const string SessionKey = "SUPABASE_SESSION";
+
+        public void DestroySession()
+        {
+        }
+
+        public Session LoadSession()
+        {
+            return new Session();
+        }
+
+        public void SaveSession(Session session)
+        {
+        }
+    }
+}
