491f6bae5dd6ff0f88abfee7891fd708d86235ec
MartinSvanholm <mart373e@gmail.com>
2025-09-04 11:36:24 +0200

    Implemented logout

---
+++ MealWiz/MealWiz/MealWiz.Shared/Features/Authentication/Logout/Logout.cs
@@ -0,0 +1,25 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.AspNetCore.Components.Authorization;
+using Supabase;
+
+namespace MealWiz.Shared.Features.Authentication.Logout;
+
+public static class Logout
+{
+    public record Command() : IRequest<Result>;
+
+    public class Handler(Client supabaseClient, AuthenticationStateProvider authenticationStateProvider) : IRequestHandler<Command, Result>
+    {
+        private readonly Client _supabaseClient = supabaseClient;
+
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var result = await Result.Try(() => _supabaseClient.Auth.SignOut());
+
+            await authenticationStateProvider.GetAuthenticationStateAsync();
+
+            return result;
+        }
+    }
+}
