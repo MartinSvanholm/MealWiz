a4efc222c9acbf9e7ac37d3bf8eef70ab5e6600d
MartinSvanholm <mart373e@gmail.com>
2025-09-05 15:40:21 +0200

    reworked create ingredient

--- MealWiz/MealWiz/MealWiz.Shared/Features/Ingredients/IngredientStateContainer.cs
+++ MealWiz/MealWiz/MealWiz.Shared/Features/Ingredients/IngredientStateContainer.cs
@@ -1,5 +1,4 @@
 ﻿using MealWiz.Shared.Features.Ingredients.EditIngredient;
-using MealWiz.Shared.Helpers;
 using MealWiz.Shared.Services.DrawerStateContainer;
 using MediatR;
 
@@ -9,7 +8,6 @@ public interface IIngredientStateContainer
 {
     Ingredient IngredientToEdit { get; set; }
     void EditIngredient(Ingredient ingredient);
-    void CreateIngredient();
 }
 
 public class IngredientStateContainer(IDrawerStateContainer _drawerStateContainer, IMediator mediator) : IIngredientStateContainer
@@ -24,20 +22,6 @@ public class IngredientStateContainer(IDrawerStateContainer _drawerStateContaine
         {
             Content = typeof(EditIngredientDialog),
             Title = "Edit Ingredient",
-            Height = "38%"
-        };
-
-        _drawerStateContainer.OpenDrawer(drawerStateParameters);
-    }
-
-    public void CreateIngredient()
-    {
-        IngredientToEdit = new();
-
-        DrawerStateParameters drawerStateParameters = new DrawerStateParameters
-        {
-            Content = typeof(EditIngredientDialog),
-            Title = "Create Ingredient",
             Height = "38%"
         };
 
