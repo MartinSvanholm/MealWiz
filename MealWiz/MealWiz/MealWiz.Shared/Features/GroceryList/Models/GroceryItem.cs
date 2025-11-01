3c429a596bd76effda9695efaf45ffe44731f3ca
MartinSvanholm <mart373e@gmail.com>
2025-11-01 08:35:34 +0100

    Implement basic grocery list page

---
+++ MealWiz/MealWiz/MealWiz.Shared/Features/GroceryList/Models/GroceryItem.cs
@@ -0,0 +1,18 @@
+﻿namespace MealWiz.Shared.Features.GroceryList.Models;
+
+public class GroceryItem
+{
+    public GroceryItem()
+    {
+        Name = string.Empty;
+        Amount = string.Empty;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Amount { get; set; }
+    public bool IsPicked { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+}
---
+++ MealWiz/MealWiz/MealWiz.Shared/Features/GroceryList/Models/GroceryList.cs
@@ -0,0 +1,15 @@
+﻿namespace MealWiz.Shared.Features.GroceryList.Models;
+
+public class GroceryList
+{
+    public GroceryList()
+    {
+        Items = [];
+    }
+
+    public int Id { get; set; }
+    public List<GroceryItem> Items { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+}
