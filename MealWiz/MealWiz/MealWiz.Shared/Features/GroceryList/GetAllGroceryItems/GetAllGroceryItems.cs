b13c51ce1746188a7b324d6485395e6b444785c5
MartinSvanholm <mart373e@gmail.com>
2025-11-10 20:49:23 +0100

    Implemented get all grocery items

---
+++ MealWiz/MealWiz/MealWiz.Shared/Features/GroceryList/GetAllGroceryItems/GetAllGroceryItems.cs
@@ -0,0 +1,22 @@
+﻿using FluentResults;
+using MealWiz.Shared.Features.GroceryList.Models;
+using MediatR;
+using Supabase;
+using Supabase.Postgrest.Responses;
+
+namespace MealWiz.Shared.Features.GroceryList.GetAllGroceryItems;
+
+public static class GetAllGroceryItems
+{
+    public record Query() : IRequest<Result<List<GroceryItem>>>;
+
+    public class Handler(Client supabaseClient) : IRequestHandler<Query, Result<List<GroceryItem>>>
+    {
+        public async Task<Result<List<GroceryItem>>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var result = await Result.Try(async Task<ModeledResponse<GroceryItemDb>> () => await supabaseClient.From<GroceryItemDb>().Get());
+
+            return result.Map(value => value.Models.ConvertAll(db => new GroceryItem(db)));
+        }
+    }
+}
--- MealWiz/MealWiz/MealWiz.Shared/Features/GroceryList/Models/GroceryItem.cs
+++ MealWiz/MealWiz/MealWiz.Shared/Features/GroceryList/Models/GroceryItem.cs
@@ -8,11 +8,22 @@ public class GroceryItem
         Amount = string.Empty;
     }
 
+    public GroceryItem(GroceryItemDb groceryItemDb)
+    {
+        Id = groceryItemDb.Id;
+        Name = groceryItemDb.Name;
+        Amount = groceryItemDb.Amount ?? string.Empty;
+        IsPicked = groceryItemDb.IsPicked;
+        CreatedBy = groceryItemDb.CreatedBy;
+        CreatedAt = groceryItemDb.CreatedAt;
+        UpdatedAt = groceryItemDb.UpdatedAt;
+    }
+
     public int Id { get; set; }
     public string Name { get; set; }
     public string Amount { get; set; }
     public bool IsPicked { get; set; }
     public Guid CreatedBy { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public DateTime UpdatedDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 }
