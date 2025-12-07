fe3bd87e0dc0d77d4d16f1c98ba8ee6244a8649a
MartinSvanholm <mart373e@gmail.com>
2025-12-07 08:59:46 +0100

    wip

---
+++ MealWiz/MealWiz/MealWiz.Shared/Helpers/DictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace MealWiz.Shared.Helpers;
+
+public static class DictionaryExtensions
+{
+    public static void Add<Tkey, TValue>(this Dictionary<Tkey, List<TValue>> keyValuePairs, Tkey key, TValue value)
+    {
+        if (keyValuePairs.TryGetValue(key, out var list))
+        {
+            list.Add(value);
+        } else
+        {
+            keyValuePairs[key] = [value];
+        }
+    }
+}
